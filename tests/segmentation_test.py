e407d804b88c1b09352dd9bebd5d37ba258b346f
Robert Ridley <robert.n.ridley@gmail.com>
2018-11-13 01:40:41 +0800

    fix import issue for test

--- tests/segmentation_test.py
+++ tests/segmentation_test.py
@@ -1,5 +1,12 @@
-import helper_methods.dictionary_methods as dictionary_methods
-import helper_methods.segmenting_methods as segmenting_methods
+import sys
+import os
+
+PACKAGE_PARENT = '..'
+SCRIPT_DIR = os.path.dirname(os.path.realpath(os.path.join(os.getcwd(), os.path.expanduser(__file__))))
+sys.path.append(os.path.normpath(os.path.join(SCRIPT_DIR, PACKAGE_PARENT)))
+
+from helper_methods import dictionary_methods as dictionary_methods
+from helper_methods import segmenting_methods as segmenting_methods
 import models
 import re
 
